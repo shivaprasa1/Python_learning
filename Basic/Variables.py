09ec7fc196524032caa4cf2c466461a90a81e24a
Shiva Prasad R <142129928+shivaprasa1@users.noreply.github.com>
2026-03-29 10:25:05 +0530

    Create Variables.py

---
+++ Basic/Variables.py
@@ -0,0 +1,32 @@
+# variable : Identifiers which can identify the values at run time 
+
+
+#1) 
+a=10                                   a=10.555                                a="shivu" 
+print(a)                               print(a)                                print(a)
+print(type(a)) --><class 'int'>        print(type(a))--><class 'float'>        print(type(a))--><class 'str'>
+
+
+#2)
+a,b,c=1,2,3  
+
+#3) 
+d    ---> d=None
+print(d) #erorr  ---> d to None not gives error
+
+#4) 
+a,b=10,20
+a,b=b,a      ----> swapping
+
+#5)
+x=y=z = 100    ----> assaing same value
+
+
+
+
+
+
+
+
+
+
