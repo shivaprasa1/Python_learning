9ef2ac9aa576b7965ac49825b3e396637546b311
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-01 10:04:56 +0530

    upadate

--- Basic/3Functions.py
+++ Basic/3Functions.py
@@ -58,3 +58,8 @@ def check_prime(num):   #---->positionel arguments
     print(num,'prime number')
 
 check_prime(39)
+
+
+#2) def function to check number is a palindrome or not
+#3) def function to print max among 3 integers
+#4) def functions to check date is valid are not , take three parameters dd-mm-yy
