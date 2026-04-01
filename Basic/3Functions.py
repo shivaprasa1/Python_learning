eb49c3cab8e346f152a6716517ceaf0554fc3a14
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-01 09:46:05 +0530

    update

--- Basic/3Functions.py
+++ Basic/3Functions.py
@@ -1,10 +1,46 @@
 #FUNCTION
+#Its is block of statements that does specific task
+
 
 #Advantage
 
+"""
 #1)Code reuseabilty - write once use many time
 #2)same task deff values
 #3)easy to modify
 #4)Modularity - code becomes shorter
 #5)Readabilty - easy to understand
-#6)Maintainabilty
+#6)Maintainabilty"""
+
+
+
+#types
+"""
+1)Builtin 
+2)user defined
+3)Ananymous (lambda)
+"""
+
+
+
+# syntax 
+
+#define function using def keyword . A function might take input in the form of parameters.
+
+"""
+def function_name ( parameters ):  --------> function declaretion (called functions)
+    #statements             ------> body
+    return expression 
+
+
+greet() ---------> calling statements
+"""
+
+
+#arguments or type of arguments
+"""
+1)posistionel
+2)keyword
+3)default
+4)variable length arg
+"""
