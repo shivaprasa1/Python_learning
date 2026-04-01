b0bd2fe21b76611979d551ecfd8c5169ea387ed5
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-01 09:23:31 +0530

    update

---
+++ Basic/3Functions.py
@@ -0,0 +1,9 @@
+#FUNCTION
+
+#Advantage
+
+#1)Code reuseabilty - write once use many time
+#2)same task deff values
+#3)easy to modify
+#4)Modularity - code becomes shorter
+#5)Readabilty - easy to understand
