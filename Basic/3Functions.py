39024ee8f9bbcc1b4a12d9b34b764d0aceb28513
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-01 10:02:08 +0530

    upadte

--- Basic/3Functions.py
+++ Basic/3Functions.py
@@ -1,9 +1,7 @@
 #FUNCTION
 #Its is block of statements that does specific task
 
-
 #Advantage
-
 """
 #1)Code reuseabilty - write once use many time
 #2)same task deff values
@@ -13,8 +11,7 @@
 #6)Maintainabilty"""
 
 
-
-#types
+#types of functions
 """
 1)Builtin 
 2)user defined
@@ -22,16 +19,13 @@
 """
 
 
-
 # syntax 
-
 #define function using def keyword . A function might take input in the form of parameters.
 
 """
 def function_name ( parameters ):  --------> function declaretion (called functions)
     #statements             ------> body
     return expression 
-
 
 greet() ---------> calling statements
 """
@@ -44,3 +38,23 @@ greet() ---------> calling statements
 3)default
 4)variable length arg
 """
+
+#example
+"""def greet():
+    print("hello")
+
+greet()"""
+
+
+#1) Prime or not in function
+def check_prime(num):   #---->positionel arguments
+    if num<=1:
+        print(num,"not a prime")
+        return
+    for i in range(2,int(num**2)+1):
+        if num%i==0:
+            print(num,"not prime")
+            return
+    print(num,'prime number')
+
+check_prime(39)
