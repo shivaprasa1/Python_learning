e7b3cc2f68a00fab7e686d7ce569172a37425c1e
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-07 11:15:08 +0530

    update

--- Basic/5Data_stracture.py
+++ Basic/5Data_stracture.py
@@ -32,6 +32,7 @@ for i in range(len(lst)-1 , -1 , -1):
 #add
 #1) By indexing 
 #2) By append
+#3) By insert
 
 
 #remove 
@@ -39,6 +40,59 @@ for i in range(len(lst)-1 , -1 , -1):
 #2) By pop - not specify it remove last or if specify then it remove that index specify
 #3) By del - not specify it remove entire object or list only 
 #4) By clear - not specify it remove only values not object
+
+#Copy or create duplicate
+#1) By Copy
+#2) By Sliceing
+#3) By loop
+
+#Sort
+#1)sort - it is method that sort originel obj
+#2)sorted - it is function that create separet obj and then sort
+
+
+#reverse 
+"""ls.reverse()"""
+
+
+#Combain two list 
+#1) By extend
+#2) By cancat or (+)
+#3) By append (only one)
+
+
+#Python function 
+#1)len()
+#2)max()
+#3)min()
+#4)sum()
+#5)sorted()
+
+
+#Slicing on list
+ls=[1,2,3,4,5,6,7,8]
+print(ls[::2])
+print(ls[1::2])
+print(ls[:])
+print(ls[::-1])
+print(ls[len(ls)-1:-1:-1]) # it gives empty becaouse -1 act as last negative index
+print(ls[len(ls)-1::-1]) 
+
+
+
+
+
+
+
+
+
+#extend vs append vs + 
+a=[1,2,3]
+b=[5,6,7]
+a.extend(b) # iterable each value and store
+a.append(b) # it store that obj as value 
+c=a+b # create new and combain
+
 
 
 
