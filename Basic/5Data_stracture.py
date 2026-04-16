d8bf6f5c8398e934fd9697af7d143c69ad025b04
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-16 10:50:42 +0530

    update

--- Basic/5Data_stracture.py
+++ Basic/5Data_stracture.py
@@ -14,7 +14,7 @@
 #(iv)dict
 
 
-#1) List - it is an linear data structure, store multiple values to single variable , also multi datatype , orderd , mutable ,duplicates allowed..............
+#1) List - it is an linear data structure, store multiple values to single variable , also multi datatype , orderd , mutable ,duplicates allowed , flixable size , ..............
 
 
 #creating empty list
@@ -30,30 +30,103 @@ for i in range(len(lst)-1 , -1 , -1):
     
 # lst=[2,5,6,6,9]
 
+#adding element
+#1) By indexing  -  assaign a value ----> lst[0]=1
+#2) By append    -  it add only ONE element at end --------> lst.append([1,2,3,5]) ---> [1,2,3,5]
+#3) By insert    -  insert element at a specific position -----> lst.insert(idx,value) 
+#4) By Extend    -  add multiple element ----> lst.extend([1,2,3,5]) ----> 1,2,3,5 
+
+
+
+"""
 #add
-#1) By indexing 
-#2) By append
-#3) By insert
+lst[0]=20000
+print(lst)
+lst.append(10000000)
+print(lst)
 
+lst=[1,2,3,4,5]
 
-#remove 
-#1) By remove - specify th e value not index
-#2) By pop - not specify it remove last or if specify then it remove that index specify
+lst.append([100,400,500,600])
+print(lst)
+
+l1=[200,300,700,800,900,1000]
+l2=list()
+l2.extend(l1)
+print(l2)
+
+l2.insert(0,100)
+print(l2)
+l2.insert(3,400)
+print(l2)
+"""
+
+#removeing elements
+#1) By remove - remove first occurence value not index  ----> lst.remove(value)
+#2) By pop -  remove and return ele (default last is remove)---->lst.pop(idx)
 #3) By del - not specify it remove entire object or list only 
-#4) By clear - not specify it remove only values not object
+#4) By clear - it remove all ele not object 
+
+
+"""ls=[1,2,3,1]
+
+ls.remove(2)
+print(ls)
+l=ls.copy()
+l1=list()
+
+l1.append(ls.pop())
+print(l)
+print(l1==l)
+
+l1.clear()
+print(l1)
+
+del ls[0]
+print(ls)"""
+
 
 #Copy or create duplicate
 #1) By Copy
 #2) By Sliceing
 #3) By loop
 
+"""
+ls=[5,4,6,8]
+c=ls.copy()
+print(c == ls)
+print(c is ls)
+
+c1=ls[:]
+print(c1)
+
+list=list()
+for i in ls:
+    list.append(i)
+print(list)
+ 
+"""
+
 #Sort
 #1)sort - it is method that sort originel obj
 #2)sorted - it is function that create separet obj and then sort
+#3) reverse - 
+
+"""ls=[1,2,3,4,5]
+print(ls.sort(key=None,reverse=True))
+print(ls.reverse())
+print(ls.copy())
+"""
 
 
-#reverse 
-"""ls.reverse()"""
+#searching or count
+"""#1) index -  search the index and also where to start to find if having same element ---> ls.index(ele,start,end)
+ls=[1,2,3,4,5,6,5,6,5,66,66,66,66,88,99,5]
+print(ls.index(5,5,8))
+
+#2)count- count occurence
+print(ls.count(5))
+"""
 
 
 #Combain two list 
@@ -62,12 +135,7 @@ for i in range(len(lst)-1 , -1 , -1):
 #3) By append (only one)
 
 
-#Python function 
-#1)len()
-#2)max()
-#3)min()
-#4)sum()
-#5)sorted()
+
 
 
 #Slicing on list
@@ -88,31 +156,6 @@ b=[5,6,7]
 a.extend(b) # iterable each value and store
 a.append(b)""" # it store that obj as value 
 # c=a+b # create new and combain
-
-
-
-
-"""
-#add
-lst[0]=20000
-print(lst)
-lst.append(10000000)
-print(lst)
-
-
-#delete or remove
-print(lst.remove(6)) # not return the value ----> None
-lst.remove(6)
-print(lst)
-print(lst.pop())   # delete and return the value
-print(lst)
-del lst[0]
-print(lst)
-#del lst # delete obj
-lst.clear() # delete values
-print(lst)"""
-
-
 
 #Nested List
 
@@ -137,8 +180,33 @@ for i in ls:
 
 # print(chr(65),chr(97))
 
+#Python function 
+#1)len()
+#2)max()
+#3)min()
+#4)sum()
+#5)sorted()
+
+#type_conversion
+#1)list(iterable)
+#2)tuple(list)
+#3)set(list)
+
+#itration_helpers
+#1)enumerate(list)
+#2)zip(list1,list2)
 
 
+list1=[1,2,3,4,5,6]
+list2=[7,8,9,10]
+
+v=enumerate(list1)
+print(list(v))
+
+for i,x in v:
+    print(x) 
+
+print(list(zip(list1,list2)))
 
 
 
@@ -154,7 +222,25 @@ for i in ls:
 
 
 
-#tuple
+#tuple -  it is an linear data structure, store multiple values to single variable , also multi datatype , orderd , immutable ,duplicates allowed , fixed size ,  ..............
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -285,10 +371,27 @@ s1.intersection(s2)
 s1&s2
 
 #difference
+s1-s2
+
+#symmentic_differnce
+s1^s2
 
 
 """
 
+a={1,2,3,4}
+b={1,2,3,4}
+c={4,3,2,1}
+
+print(a==b,a>=b,a<=c)
+
+
+#checking
+
+#superset  
+
+#subset
+#disjoint
 
 
 
